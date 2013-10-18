ec5083709300739083d52cea680be005a1700bbc
Robert Marshall <lucasmarshall@Lukes-MacBook-Air.local>
2013-10-18 11:41:09 -0700

    Clean up application

--- application.py
+++ application.py
@@ -14,37 +14,44 @@ app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 
 @app.route("/")
-def map():
+def mapview():
     return render_template("map.html", api_key=app.config['GOOGLE_API_KEY'])
 
 @app.route("/foodtrucks", methods=['GET'])
 def trucks():
 	if request.method == 'GET':
 		query = SodaQuery('http://data.sfgov.org/resource/rqzj-sfat.json') \
-					.select('objectid', 'applicant', 'fooditems', 'latitude', 'longitude', 'location', 'address') \
-					.where(facilitytype = 'Truck', approved__null = False, latitude__null = False, longitude__null = False, address__null = False)
+				.select('objectid', 'applicant', 'fooditems', 'latitude', 'longitude', 'location', 'address') \
+				.where( # get only food trucks, and filter out incomplete records
+					facilitytype = 'Truck',
+					approved__null = False,
+					latitude__null = False,
+					longitude__null = False,
+					address__null = False
+				)
 
+		# @todo
 		# Unfortunately, it looks like the within_box api on the data.sfgov.org site doesn't work, at least for this dataset
 		# Uncomment this when it does work, and refactor the JS to not expect a full list of trucks as it does now
+		# You'll also want to revisit caching stragegies as the query-based one implemented in the soda library won't cut it.
 		#location_box = request.args.get('location_box', None)
 		#if location_box:
 		#	query = query.where(location__within_box=location_box.split(','))
 
-
-		data = query.execute()
-
-		response = []
-		for truck in data:
-			response.append({
-				'id': truck['objectid'],
-				'name': truck['applicant'],
-				'type': truck['fooditems'],
-				'lat': truck['latitude'],
-				'lng': truck['longitude'],
-				'address': truck['address']
-			})
+		data     = query.execute()
+		response = map(__doResponseMapping, data);
 
 		return json.dumps(response), 200, {'Content-Type' : 'application/json'}
+
+def __doResponseMapping(item):
+	return {
+		'id': item['objectid'],
+		'name': item['applicant'],
+		'type': item['fooditems'],
+		'lat': item['latitude'],
+		'lng': item['longitude'],
+		'address': item['address']
+	}
 
 if __name__ == '__main__':
 	app.run()
